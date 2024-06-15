2e17c360025f599ee2c0d670511499861c1f57fe
lishuo19981027 <1967838808@qq.com>
2024-06-15 20:56:59 +0800

    学习微服务通信

--- e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/NacosClientApplication.java
+++ e-commerce-alibaba-nacos-client/src/main/java/com/imooc/ecommerce/NacosClientApplication.java
@@ -3,10 +3,14 @@ package com.imooc.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-    /*
+/*
     * Nacos Client 工程启动入口
     * */
+@EnableFeignClients
+@RefreshScope //刷新配置
 @EnableDiscoveryClient
 @SpringBootApplication
 public class NacosClientApplication {
